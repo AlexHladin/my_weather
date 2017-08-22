2f249b947118b02afbae0095e7be44b13cb4f33a
AlexGladin <alexgladin17@gmail.com>
2017-08-22 14:39:04 +0300

    City selection

--- public/js/auto_inputbox.js
+++ public/js/auto_inputbox.js
@@ -3,6 +3,7 @@
     var methods={
         init:function(options) {
             options=$.extend({
+                icon: 'myautoInputIcon',
       	        className: 'autoInput',
                 closeIcon: 'fa-times-circle',
                 loadingIcon: 'fa-spinner fa-pulse',
@@ -13,17 +14,20 @@
                     return $(el).html();
                 },
                 slideDownComplete: function() {
+                },
+                onSubmit: function(event, value) {
+
                 }
    	        }, options || {});
 
-            return this.each(function(){
+            return this.each(function() {
                 $.data(this, 'myautoInput', options);
 
-                $(this).after('<div class="' + options.className + '-content"><ul></ul></div><i class="loading fa ' + options.loadingIcon + ' fa-lg" aria-hidden="true"></i>');
+                $(this).after('<div class="' + options.className + '-content"><ul></ul></div><i id="' + options.icon + '" class="loading fa ' + options.loadingIcon + ' fa-lg" aria-hidden="true"></i>');
 
                 // if input not empty show icon to delete text in input
                 if ($(this).val()) {
-                    $(this).parent().find('i')
+                    $(this).parent().find('#' + options.icon)
                         .removeClass(options.loadingIcon)
                         .addClass(options.closeIcon)
                         .css('visibility', 'visible');
@@ -32,10 +36,11 @@
                 $(this).parent()
                     .find('.' + options.className + '-content')
                     .css('width', $(this).css('width')) // set width of drop-down list equivalent to input with
-                    .css('left', this.getBoundingClientRect().left); // fix left position of drop-down list to input
+                    .css('top', this.getBoundingClientRect().top + 4);
+                    //.css('left', this.getBoundingClientRect().left); // fix left position of drop-down list to input
 
                 // add event listener to empty icon
-                $(this).parent().find('i').bind('click', function() {
+                $(this).parent().find('#' + options.icon).bind('click', function() {
                     $(this).parent().find('input').val('');
                     $(this).css('visibility', 'hidden');
                 });
@@ -43,7 +48,12 @@
    	            $(this).bind('click', function() {
                     return false;  
                 }).bind('textchange', function() {
-                    methods.load.apply($(this));
+                    if (this.value) methods.load.apply($(this));
+                });
+
+                $(this).parent().parent().submit(function(event) {                   
+                    options.onSubmit(event, $(this).find('input').val());
+                    methods.hide.apply($('.' + options.className + '-content').parent());
                 });
 
                 $(document).click(function(){
@@ -68,14 +78,16 @@
 	      load: function() {
    	        return this.each(function() {
                 var options = $.data(this, 'myautoInput');
+
                 var autoInputElem = this;
+                var iconElem = $(this).parent().find('#' + options.icon);
+                
+                if (iconElem.css('visibility') == 'hidden')
+                    iconElem.css('visibility', 'visible');
 
-                if ($(this).parent().find('i').css('visibility') == 'hidden')
-                    $(this).parent().find('i').css('visibility', 'visible');
-
-                if ($(this).parent().find('i').hasClass(options.closeIcon)) {
-                    $(this).parent().find('i').removeClass(options.closeIcon);
-                    $(this).parent().find('i').addClass(options.loadingIcon);
+                if (iconElem.hasClass(options.closeIcon)) {
+                    iconElem.removeClass(options.closeIcon);
+                    iconElem.addClass(options.loadingIcon);
                 }
               
                 $.ajax({
@@ -84,48 +96,52 @@
                     dataType: 'json',                
                     success: function(data) {                        
                         $(autoInputElem).next().find('ul').empty();
-                        $.each(data, function(index, el) {
-                            var formatedElement = options.formatElement(el);
-                            
-                            if (formatedElement.toLowerCase().indexOf($(autoInputElem).val().toLowerCase()) >= 0)
-                                $(autoInputElem).next().find('ul').append('<li>' + formatedElement + '</li>');
-                        });
+                        // check if response contain rows
+                        if (data.length) {
+                            $.each(data, function(index, el) {
+                                var formatedElement = options.formatElement(el);
+                                
+                                if (formatedElement.toLowerCase().indexOf($(autoInputElem).val().toLowerCase()) >= 0)
+                                    $(autoInputElem).next().find('ul').append('<li>' + formatedElement + '</li>');
+                            });
 
-                        $(autoInputElem).next().find('li').on('click', function() {
-                            var ret = options.onItemSelected(this);
-                            // console.log($(autoInputElem).attr('class'));
-                            $(autoInputElem).val(ret);
-                            if (ret) {
-                                $(autoInputElem).parent().parent().submit();
-                            }
-                        });
+                            $(autoInputElem).next().find('li').on('click', function() {
+                                var ret = options.onItemSelected(this);
+                                
+                                if (ret && ret != false) {
+                                    $(autoInputElem).val(ret);
+                                    $(autoInputElem).parent().parent().submit();
+                                }
+                            });
 
-                        $(autoInputElem).parent().find('i').removeClass(options.loadingIcon);
-                        $(autoInputElem).parent().find('i').addClass(options.closeIcon);
+                            iconElem.removeClass(options.loadingIcon);
+                            iconElem.addClass(options.closeIcon);
 
-                        methods.show.apply(autoInputElem);
-		                },
-          		      error: function(jqXHR, textStatus, errorThrown) {
+                            methods.show.apply(autoInputElem);
+                        } else {
+                            methods.hide.apply($('.' + options.className + '-content').parent());
+                        }
+                    },
+          		    error: function(jqXHR, textStatus, errorThrown) {
                         methods.show.apply(autoInputElem);
                         $(autoInputElem).next().find('ul').empty();
                         $(autoInputElem).next().find('ul').append('<div class="text-center">Nothing found</div>');
                         
-                        $(autoInputElem).parent().find('i').removeClass(options.loadingIcon);
-                        $(autoInputElem).parent().find('i').addClass(options.closeIcon);
-          		      }
+                        iconElem.removeClass(options.loadingIcon);
+                        iconElem.addClass(options.closeIcon);
+                    }
    	            });
 	        });
           }
     };
 
-	  $.fn.autoInput=function(method){
-       	if ( methods[method] ) {
-         		return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
-       	} else if ( typeof method === 'object' || ! method ) {
-            
-         		return methods.init.apply( this, arguments );
-     		} else {
-            $.error( 'Method ' +  method + ' does not exist on jQuery.autoInput' );
+    $.fn.autoInput=function(method) {
+    	if (methods[method]) {
+     		return methods[method].apply( this, Array.prototype.slice.call(arguments, 1));
+    	} else if (typeof method === 'object' || ! method) {
+            return methods.init.apply(this, arguments);
+    	} else {
+            $.error('Method ' +  method + ' does not exist on jQuery.autoInput');
         }    
-	  }
+    }
 })();
